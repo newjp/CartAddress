9e01533e7f919274719bb3996f31ea270a8a9111
newjp <newjp@foxmail.com>
2020-09-01 04:19:06 +0800

    过滤器的使用

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -68,7 +68,7 @@
               <li v-for="item in cartList" :key="item.productId">
                 <div class="cart-tab-1">
                   <div class="cart-item-check">
-                    <a href="javascipt:;" class="checkbox-btn item-check-btn" v-bind:class="{'checked': item.checked}">
+                    <a href="javascipt:;" class="checkbox-btn item-check-btn" v-bind:class="{'checked': item.checked}" @click="editCart('checked', item)">
                       <svg class="icon icon-ok">
                         <use xlink:href="#icon-ok" />
                       </svg>
@@ -88,15 +88,15 @@
                   <div class="item-quantity">
                     <div class="select-self select-self-open">
                       <div class="select-self-area">
-                        <a class="input-sub">-</a>
+                        <a class="input-sub" v-on:click="editCart('minus', item)">-</a>
                         <span class="select-ipt">{{item.productNum}}</span>
-                        <a class="input-add">+</a>
+                        <a class="input-add" v-on:click="editCart('add', item)">+</a>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div class="cart-tab-4">
-                  <div class="item-price-total">￥{{item.productPrice * item.productNum}}元</div>
+                  <div class="item-price-total">{{(item.productPrice * item.productNum) | currency}}</div>
                 </div>
                 <div class="cart-tab-5">
                   <div class="cart-item-opration">
@@ -162,14 +162,32 @@ export default {
   mounted() {
     this.init();   //初始化购物车列表
   },
+  filters: {
+    currency(value) {
+      if (!value) return 0.00;
+      return '￥' + value.toFixed(2) + '元';
+    }
+  },
   methods: {
+    //初始化购物车列表数据
     init() {
       this.axios.get("/mock/cart.json").then((response) => {
-        console.log(response);
+        //console.log(response);
         let res  = response.data;
         this.cartList = res.data;
-        console.log(this.cartList);
+        //console.log(this.cartList);
       })
+    },
+    //修改购物车数量
+    editCart(type, item){
+      //debugger;
+      if (type == 'add') {
+        item.productNum ++;
+      } else if (type == 'minus') {
+        item.productNum --;
+      } else {
+        item.checked = !item.checked;
+      }
     }
   }
 };
